171b3c6025e1c376ccacce2d9a7cecfee50a9ab2
James R. Davenport <jrdavenport85@gmail.com>
2016-03-29 22:28:28 +0000

    challenge completed v1

---
+++ README.md
@@ -0,0 +1,16 @@
+File Metadata Microservice
+=============
+https://www.freecodecamp.com/challenges/file-metadata-microservice
+
+
+Objective: Build a full stack JavaScript app that is functionally similar to this: https://cryptic-ridge-9197.herokuapp.com/ and deploy it to Heroku.
+
+
+* User Story: I can submit a FormData object that includes a file upload.
+
+* User Story: When I submit something, I will receive the file size in bytes within the JSON response
+
+
+Example output:
+
+{ "size": 1337 }
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+var fs = require('fs')
 var express = require('express')
+var multer  = require('multer')
+var upload = multer({ 
+    dest: 'uploads',
+    })
+ 
 var app = express()
 
-
+app.post('/', upload.single('sizeMe'), function (req, res, next) {
+  res.json({'size': req.file.size})
+  var path = req.file.path
+  fs.unlinkSync(path)
+  res.end()
+})
 
 app.get('/',function(req, res) {
     res.sendFile('/index.html', { root : __dirname}, function(err){
         if (err) throw err
         res.end()
     })
-   
 })
 
 app.listen(process.env.PORT)
